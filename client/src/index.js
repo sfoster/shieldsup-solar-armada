8db4be014ae642330f34c5e0056d67343b675012
Sam Foster <sam@sam-i-am.com>
2023-04-10 00:19:20 -0700

    Wire up authentication for PUTs, passing along the auth token and verifying it on the server side

--- client/src/index.js
+++ client/src/index.js
@@ -38,13 +38,24 @@ function connectClient() {
       onAuthStateChanged(this.auth, (user) => {
         console.log("onAuthStateChanged:", user);
         if (user) {
-          this.connected = true;
-          this.emit("signedin", user);
+          this.onUserAuthenticated(user);
         } else {
-          this.connected = false;
-          this.emit("signedout");
+          this.onUserLogout();
         }
       });
+    }
+    async onUserAuthenticated(user) {
+      console.assert(this.auth.currentUser == user, "user arg is auth's currentUser");
+      this.currentUserIdToken = await user.getIdToken();
+      this.currentUser = user;
+      this.connected = true;
+      this.emit("signedin", { user: this.currentUser, idToken: this.currentUserIdToken });
+    }
+    onUserLogout() {
+      this.connected = false;
+      delete this.currentUser;
+      delete this.currentUserIdToken;
+      this.emit("signedout");
     }
     logout() {
       return signOut(this.auth);
@@ -67,6 +78,35 @@ function connectClient() {
         });
       }
     }
+    updateEntity(path, data) {
+      if (!(this.connected && this.currentUser)) {
+        console.info("User not logged in and/or client not connected");
+        return;
+      }
+      const url = `/api/${path}`;
+      return this._apiRequest(url, "PUT", data);
+    }
+    async _apiRequest(url, method, payload) {
+      console.log(`Sending request to update: ${url} with: ${JSON.toString(payload)}`);
+      let resp = await fetch(url, {
+        method,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        // TODO: Use Authorization header or add the token into this request envelope?
+        body: JSON.stringify({
+          data: payload,
+          credential: `token=${this.currentUserIdToken}`,
+        })
+      });
+      if (resp.ok) {
+        console.log("Got ok response:", await resp.text());
+        // displayResult(await resp.json());
+      } else {
+        console.warn("error response:", resp);
+      }
+    }
   };
   const client = new _Client();
   client.init();
@@ -74,36 +114,69 @@ function connectClient() {
   return client;
 }
 
-function updateUI({ loggedIn } = {}) {
-  const remoteBackedElements = document.querySelectorAll("[data-remoteid]");
-  console.log("remoteBackedElements:", remoteBackedElements.length);
-  remoteBackedElements.forEach(elem => {
-    switch (elem.dataType) {
-      case "item":
-        elem.disabled = !loggedIn;
-        console.log("CE item type with remoteid", elem.dataset.remoteid);
-        break;
-      case "list": {
-        elem.disabled = !loggedIn;
-        if (loggedIn) {
-          if (!elem.collection) {
-            const collection = window[elem.id + "-model"] = new RemoteList(elem.dataset.remoteid);
-            elem.setCollection(collection);
+const UI = window.UI = new class _FormUI {
+  get gameUpdateBtn() {
+    return document.getElementById("gameUpdateBtn")
+  }
+  init(rootElem) {
+    console.log("Init UI with rootElem:", rootElem);
+    this.rootElem = rootElem;
+    this.rootElem.addEventListener("click", this);
+  }
+  update({ loggedIn } = {}) {
+    const remoteBackedElements = document.querySelectorAll("[data-remoteid]");
+    console.log("remoteBackedElements:", remoteBackedElements.length);
+    remoteBackedElements.forEach(elem => {
+      switch (elem.dataType) {
+        case "item":
+          elem.disabled = !loggedIn;
+          console.log("CE item type with remoteid", elem.dataset.remoteid);
+          break;
+        case "list": {
+          elem.disabled = !loggedIn;
+          if (loggedIn) {
+            if (!elem.collection) {
+              const collection = window[elem.id + "-model"] = new RemoteList(elem.dataset.remoteid);
+              elem.setCollection(collection);
+            }
           }
+          break;
         }
-        break;
+        default:
+          console.log("Unknown element with [remoteid]", elem);
+          break;
       }
-      default:
-        console.log("Unknown element with [remoteid]", elem);
-        break;
+    });
+  }
+  handleEvent(event) {
+    if (event.type == "click") {
+      switch (event.target) {
+        case this.gameUpdateBtn: {
+          console.log("Handling click on gameUpdateBtn");
+          const pathId = document.getElementById('datalabel').textContent.trim();
+          let data;
+          try {
+            data = JSON.parse(document.getElementById('datainput').value);
+          } catch (ex) {
+            console.warn("Bad data format, it should be valid JSON", ex);
+            return;
+          }
+          window.gameClient.updateEntity(
+            pathId, data
+          );
+          break;
+        }
+      }
     }
-  });
-}
+  }
+}();
 
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("In DOMContentLoaded");
   const client = window.gameClient = connectClient();
-  client.on("signedin", (user) => {
+  window.UI.init(document.body);
+  client.on("signedin", ({ user, idToken }) => {
+    console.log("Client signedin, got idToken:", idToken);
     console.log("Client signedin, got user:", user);
     const latest = window.theLatest = new RemoteObject("games/thelatest");
     latest.on("value", (val) => {
@@ -113,40 +186,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       }, null, 2);
       console.log(`Got value from ${latest.path}: ${JSON.stringify(val)}`);
     });
-    updateUI({ loggedIn: true });
+    window.UI.update({ loggedIn: true });
   });
   client.on("signedout", () => {
-    updateUI({ loggedIn: false });
+    window.UI.update({ loggedIn: false });
   });
 });
 
-function updateModel(model, payload) {
-  let data;
-  try {
-    data = JSON.parse(payload);
-  } catch (ex) {
-    console.warn("Bad data format, it should be valid JSON", ex);
-    return;
-  }
-  // const model = window.theLatest;
-  const url = `/api/${model.path}`;
-  return apiRequest(url, "PUT", data);
-}
-
-const apiRequest = window.apiRequest = async function(path, method, bodyData) {
-  console.log(`Sending request to update: ${url} with: ${payload}`);
-  let resp = await fetch(url, {
-    method,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(bodyData)
-  });
-  if (resp.ok) {
-    console.log("Got ok response:", await resp.text());
-    // displayResult(await resp.json());
-  } else {
-    console.warn("error response:", resp);
-  }
-}
--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-// const { getAuth } = require("firebase-admin/auth");
+const { getAuth } = require("firebase-admin/auth");
 const firebaseProjectConfig = functions.config().firebase;
 
 const express = require('express');
 const serviceAccount = require("../.secrets/api-project-482814424574-de59b6e3ffb8.json");
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
+  projectId: "demo-shieldsup-api-test",
   credential: admin.credential.cert(serviceAccount),
   // The database URL depends on the location of the database
   databaseURL: "http://localhost:9000/?ns=shieldsup-api-test"
 });
 const db = admin.database();
-// const firebaseAuth = getAuth();
 const app = express();
 // const authMiddleWare = require("firebase-auth-express-middleware");
 
@@ -28,20 +28,40 @@ function getCollection(path) {
 
 function updateDocument(path, newData) {
   console.log("updateDocument, got path:", path, newData);
+
   const ref = db.ref(path);
-  return ref.update(Object.assign({},newData, {
+  return ref.update(Object.assign({}, newData, {
     lastUpdated: Date.now(),
   }));
 }
 
-let authorized = false;
-function checkAuth(req, res, next) {
-  if (authorized) {
-    next();
-  } else {
-    res.status(403).json({ "status": "unauthorized"});
-    return;
+function checkAuth(req, resp, next) {
+  let authorized = false;
+  // NB: ID token verification requires a project ID.
+  // https://firebase.google.com/docs/auth/admin/verify-id-tokens
+  let credential = req.headers.authorization;
+  if (!credential && req.body && req.body.credential) {
+    credential = req.body.credential;
   }
+  console.log("checkAuth, trying credential:", credential);
+  if (credential) {
+    let nameValue = credential.split(/;\s*/).find(part => part.startsWith("token="));
+    const [_,idToken] = (nameValue || "").split("=");
+    if (idToken) {
+      const firebaseAuth = getAuth();
+      firebaseAuth.verifyIdToken(idToken)
+      .then(() => {
+        console.log("idToken verified");
+        next();
+      })
+      .catch(error => {
+        console.warn("error verifying token:", error);
+        resp.status(403).json({ "status": "unauthorized"});
+      });
+      return;
+    }
+  }
+  res.status(403).json({ "status": "unauthorized"});
 }
 
 app.use(express.json());
@@ -73,10 +93,13 @@ app.get(
 );
 
 app.put("/api/games/:id", async (req, resp) => {
+  if (resp.headersSent) {
+    return;
+  }
   console.log("Update game: ", req.params.id, req.body);
   let result = "Alright";
   try {
-    await updateDocument(`games/${req.params.id}`, req.body);
+    await updateDocument(`games/${req.params.id}`, req.body.data);
   } catch (ex) {
     console.warn("Failed to update:", ex);
     result = "Fail";
